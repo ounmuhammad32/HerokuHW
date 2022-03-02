1fc4dde6ad5c3312f305f1f6b6a0dd138a615488
ounmuhammad32 <45170041+ounmuhammad32@users.noreply.github.com>
2022-03-01 20:46:08 -0500

    updated About.html and created test CI/CD branch change in simple page 5

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -1,49 +1,20 @@
 """This test the homepage"""
-
 def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/about">About</a>' in response.data
-
-
-def test_request_about(client):
+    assert b'<a class="nav-link" aria-current="page" href="/about">About</a>' in response.data
+def test_request_index(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b"index" in response.data
-
+    assert b"Home" in response.data
 def test_request_about(client):
     """This makes the index page"""
     response = client.get("/about")
     assert response.status_code == 200
     assert b"About" in response.data
-
-# def test_request_page1(client):
-#     """This makes the index page"""
-#     response = client.get("/page1")
-#     assert response.status_code == 200
-#     assert b"Page 1" in response.data
-#
-# def test_request_page2(client):
-#     """This makes the index page"""
-#     response = client.get("/page2")
-#     assert response.status_code == 200
-#     assert b"Page 2" in response.data
-#
-# def test_request_page3(client):
-#     """This makes the index page"""
-#     response = client.get("/page3")
-#     assert response.status_code == 200
-#     assert b"Page 3" in response.data
-#
-# def test_request_page4(client):
-#     """This makes the index page"""
-#     response = client.get("/page4")
-#     assert response.status_code == 200
-#     assert b"Page 4" in response.data
-#
-# def test_request_page_not_found(client):
-#     """This makes the index page"""
-#     response = client.get("/page5")
-#     assert response.status_code == 404
+def test_request_page_not_found(client):
+    """This makes the index page"""
+    response = client.get("/page1")
+    assert response.status_code == 404
