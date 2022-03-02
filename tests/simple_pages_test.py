5be7c294d45dc80ddf222a8161445b90c3104038
ounmuhammad32 <45170041+ounmuhammad32@users.noreply.github.com>
2022-03-01 21:06:58 -0500

    updated About.html and created test CI/CD branch change in simple page 10

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -3,9 +3,8 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/about">About</a>' in response.data
-    assert b'<a class="nav-link" href="/page1">Page 1</a>' in response.data
-    
+    assert b'<a class="nav-link" aria-current="page" href="/about">About</a>' in response.data
+
 def test_request_index(client):
     """This makes the index page"""
     response = client.get("/")
